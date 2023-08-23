4d4c3117b823b0a551da59ba32bae89b1f3d9366
kamruzzaman sohag <shorkar.sohag74@gmail.com>
2023-08-23 14:48:39 +0600

    kkk

--- src/app/PictureShow/FlexSection.js
+++ src/app/PictureShow/FlexSection.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Image from 'next/image';
 
 const FlexSection = ({ imageUrl, heading, paragraph }) => {
   return (
     <div className="max-w-md mx-auto rounded-xl overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
       <div className="relative">
-        <img className="h-48 w-full object-cover rounded-t-xl" src={imageUrl} alt="Card" />
+        <Image className="h-48 w-full object-cover rounded-t-xl" src={imageUrl} alt="Card" width={500} height={500}/>
       </div>
       <div className="p-6 bg-white">
         <h3 className="text-2xl font-bold text-gray-800 mb-2">{heading}</h3>
